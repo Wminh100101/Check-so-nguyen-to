a1730c81f17907342dff891e08584016c8f11dda
MinhTGN <90247435+MinhTGN@users.noreply.github.com>
2021-09-23 17:21:53 +0700

    add code

---
+++ src/com/white/main.java
@@ -0,0 +1,22 @@
+package com.white;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Xin mời nhập số cần kiểm tra:");
+		int a = sc.nextInt();
+		
+		caculate ca = new caculate(a);
+		
+		if(ca.check) {
+			System.out.print(a +" là số nguyên tố");
+		}
+		else {
+			System.out.print(a +" không phải là số nguyên tố");
+		}
+	}
+
+}
